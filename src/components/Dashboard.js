3383f1d5032e05b4992468e6c0827c631c6d9c4e
PaolaP <paolaperez8@gmail.com>
2022-03-26 21:19:16 -0400

    focus state using filter

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,45 +8,54 @@ const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    value: 6,
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    value: "1pm",
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    value: "Wednesday",
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
-  }
+    value: "2.3",
+  },
 ];
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null,
   };
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+     });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map(panel => (
-      <Panel
-        key={panel.id}
-        id={panel.id}
-        label={panel.label}
-        value={panel.value}
-      />
-    ));
+    const panels = data
+      .filter(
+        (panel) =>
+          this.state.focused === null || this.state.focused === panel.id
+      )
+
+      .map((panel) => (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+        />
+      ));
 
     return <main className={dashboardClasses}>{panels}</main>;
   }
